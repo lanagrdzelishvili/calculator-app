deb77893a48ef7177a191b375288742e686d84a9
lana fox <lana@gmail.com>
2023-07-11 22:53:32 +0400

    theme

--- js.js
+++ js.js
@@ -123,3 +123,63 @@ deletenButtons.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
 })
+
+
+const themeNumbers = document.querySelectorAll('.theme-numbers p');
+
+function changeTheme(theme) {
+    const root = document.documentElement;
+
+    if (theme === '1') {
+        root.style.setProperty('--first-bg', 'hsl(222, 26%, 31%)');
+        root.style.setProperty('--kaypad-bg', 'hsl(223, 31%, 20%)');
+        root.style.setProperty('--screen-bg', 'hsl(224, 36%, 15%)');
+        root.style.setProperty('--key-bg', 'hsl(225, 21%, 49%)');
+        root.style.setProperty('--key-shadow', 'hsl(224, 28%, 35%)');
+        root.style.setProperty('--key-toggle', 'hsl(6, 63%, 50%)');
+        root.style.setProperty('--key-shadow-red', 'hsl(6, 70%, 34%)');
+        root.style.setProperty('--key-bg-light', 'hsl(30, 25%, 89%)');
+        root.style.setProperty('--key-Grayish-orange', 'hsl(28, 16%, 65%)');
+        root.style.setProperty('--Very-dark-grayish-blue', 'hsl(221, 14%, 31%)');
+        root.style.setProperty('--White', 'hsl(0, 0%, 100%)');
+    } else if (theme === '2') {
+        root.style.setProperty('--first-bg', 'hsl(0, 0%, 90%)');
+        root.style.setProperty('--kaypad-bg', 'hsl(0, 5%, 81%)');
+        root.style.setProperty('--screen-bg', 'hsl(0, 0%, 93%)');
+        root.style.setProperty('--key-bg', 'hsl(185, 42%, 37%)');
+        root.style.setProperty('--key-shadow', 'hsl(185, 58%, 25%)');
+        root.style.setProperty('--key-toggle', 'hsl(25, 98%, 40%)');
+        root.style.setProperty('--key-shadow-red', 'hsl(25, 99%, 27%)');
+        root.style.setProperty('--key-bg-light', 'hsl(45, 7%, 89%)');
+        root.style.setProperty('--key-Grayish-orange', 'hsl(35, 11%, 61%)');
+        root.style.setProperty('--Very-dark-grayish-blue', 'hsl(60, 10%, 19%)');
+        root.style.setProperty('--White', 'hsl(0, 0%, 100%)');
+    } else if (theme === '3') {
+        root.style.setProperty('--first-bg', 'hsl(268, 75%, 9%)');
+        root.style.setProperty('--kaypad-bg', 'hsl(268, 71%, 12%)');
+        root.style.setProperty('--screen-bg', 'hsl(268, 71%, 12%)');
+        root.style.setProperty('--key-bg', 'hsl(281, 89%, 26%)');
+        root.style.setProperty('--key-shadow', 'hsl(285, 91%, 52%)');
+        root.style.setProperty('--key-toggle', 'hsl(177, 92%, 70%)');
+        root.style.setProperty('--key-shadow-red', 'hsl(268, 47%, 21%)');
+        root.style.setProperty('--key-bg-light', 'hsl(268, 47%, 21%)');
+        root.style.setProperty('--key-Grayish-orange', 'hsl(290, 70%, 36%)');
+        root.style.setProperty('--Very-dark-grayish-blue', 'hsl(52, 100%, 62%)');
+        root.style.setProperty('--White', 'hsl(0, 0%, 100%)');
+    }
+}
+
+
+themeNumbers.forEach((themeNumber, index) => {
+    themeNumber.addEventListener('click', () => {
+        const selectedTheme = index + 1;
+        changeTheme(selectedTheme.toString());
+    });
+});
+
+const rangeInput = document.getElementById('rangeInput');
+
+rangeInput.addEventListener('input', () => {
+    const selectedTheme = rangeInput.value;
+    changeTheme(selectedTheme);
+});
